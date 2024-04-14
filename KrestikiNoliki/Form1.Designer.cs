b5b0816e8c9dda5395500bfef434d194b120f453
Koledloda <matveikoledin070306@gmail.com>
2024-04-14 22:24:59 +0300

    Добавьте файлы проекта.

---
+++ KrestikiNoliki/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace KrestikiNoliki
+{
+    partial class Forma
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Forma
+            // 
+            this.ClientSize = new System.Drawing.Size(533, 279);
+            this.Name = "Forma";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Panel0_1;
+        private System.Windows.Forms.Panel Panel0_3;
+        private System.Windows.Forms.Panel Panel0_2;
+        private System.Windows.Forms.Panel Panel1_5;
+        private System.Windows.Forms.Panel Panel1_6;
+        private System.Windows.Forms.Panel Panel1_4;
+        private System.Windows.Forms.Panel Panel2_8;
+        private System.Windows.Forms.Panel Panel2_9;
+        private System.Windows.Forms.Panel Panel2_7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KrestikiNoliki/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrestikiNoliki
+{
+    public partial class Forma : System.Windows.Forms.Form
+    {
+        List<Button> buttons = new List<Button>();
+        string KN;
+        public Forma()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void RandomKN()
+        {
+            var p = new Random();
+            KN = p.Next(0, 2) == 0 ? "X" : "O";
+        }
+
+        void ButtonsInit()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons.Add(new Button
+                    {
+                        Size = new Size(200, 200),
+                        Location = new Point(i * 200, j * 200),
+                        TabStop = false,
+                        Font = new Font("Timse New Roman", 80),
+                    });
+                    buttons[buttons.Count - 1].Click += ButtonClick;
+                    Controls.Add(buttons[buttons.Count - 1]);
+                }
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            var currentButton = sender as Button;
+            currentButton.Text = KN;
+            currentButton.Click -= ButtonClick;
+            KN = KN == "X" ? "O" : "X";
+            if (CheckForWinner("X"))
+            {
+                MessageBox.Show("Победили крестики!");
+                ResetGame();
+            }
+            else if (CheckForWinner("O"))
+            {
+                MessageBox.Show("Победили нолики!");
+                ResetGame();
+            }
+            else if (CheckForWinner("X") || CheckForWinner("O"))
+            {
+                MessageBox.Show("Ничья!");
+                ResetGame();
+            }
+        }
+
+        void Init()
+        {
+            ClientSize = new Size(600, 600);
+            ButtonsInit();
+            RandomKN();
+        }
+
+       
+        private bool CheckForWinner(string symbol)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i * 3].Text == symbol && buttons[i * 3 + 1].Text == symbol && buttons[i * 3 + 2].Text == symbol)
+                    return true;
+                if (buttons[i].Text == symbol && buttons[i + 3].Text == symbol && buttons[i + 6].Text == symbol)
+                    return true;
+            }
+            if (buttons[0].Text == symbol && buttons[4].Text == symbol && buttons[8].Text == symbol)
+                return true;
+            if (buttons[2].Text == symbol && buttons[4].Text == symbol && buttons[6].Text == symbol)
+                return true;
+
+            return false;
+        }
+
+        private void ResetGame()
+        {
+            foreach (Button button in buttons)
+            {
+                button.Text = "";
+                button.Click += ButtonClick;
+            }
+            RandomKN();
+        }
+    }
+}
